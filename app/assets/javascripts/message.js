5d90297b003c141e41cc7730724579a314ee81f8
河合宏紀 <hironori.kawai0720@gmail.com>
2019-08-11 14:16:09 +0900

    message asynchronous

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,57 @@
+$(function() {
+  
+    // 画面上の最新ID
+    var latest_id = 0;
+  
+    function buildHTML(message){
+      var MessageImage = ``
+      if (message.image){
+        MessageImage = `<img class="lower-message__image" src="${message.image}">`
+      }
+      var html = ` <div class= "message" "data-message-id"=${message.id}>
+                      <div class= upper-message>
+                        <div class= upper-message__user-name>
+                          ${message.user_name}
+                        </div>
+                        <div class= upper-message__date>
+                          ${message.created_at}
+                        </div>
+                      </div>
+                      <div class= lower-message>
+                        <div class= lower-message__content>
+                          ${message.content}
+                        </div>
+                          ${MessageImage}
+                      </div>
+                  </div>`
+      return html;
+    }
+  
+    $('#new_message').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var href = window.location.href + ''
+      $('.form__submit').removeAttr('data-disable-with');
+  
+      $.ajax({
+        url: href,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false,
+        timeout: 10000
+      })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $('.form__message').val('');
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, "first");
+        // 画面上の最新IDを更新
+        latest_id = data.id;
+      })
+      .fail(function(){
+        alert('非同期通信に失敗しました');
+      });
+    });
+  });
